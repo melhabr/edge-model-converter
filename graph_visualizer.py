d7e43a7d0b84ff9b2c84b374ab4af3c11d2a4676
Mason El-Habr <melhabr@gmail.com>
2020-07-01 22:00:37 -0400

    Added port argument

--- graph_visualizer.py
+++ graph_visualizer.py
@@ -1,23 +1,19 @@
 import tensorflow as tf
 import argparse
 import subprocess
-import webbrowser
-
-def visualize_graph(graph_def):
-
-    with tf.compat.v1.Session() as sess:
-        tf.import_graph_def(graph_def, name='')
-        writer = tf.compat.v1.summary.FileWriter("./graphs", sess.graph)
-
-    subprocess.run(["tensorboard", "--logdir", "./graphs"])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", "-i", help="Path to input file", required=True, type=str)
+    parser.add_argument("--input", "-i", help="Path to input file", type=str)
+    parser.add_argument("--port", "-p", help="Port to use", default=6006, type=int)
     args = parser.parse_args()
 
     with tf.compat.v1.gfile.GFile(args.input, "rb") as f:
         graph_def = tf.compat.v1.GraphDef()
         graph_def.ParseFromString(f.read())
 
-    visualize_graph(graph_def)
+    with tf.compat.v1.Session() as sess:
+        tf.import_graph_def(graph_def, name='')
+        writer = tf.compat.v1.summary.FileWriter("./graphs", sess.graph)
+
+    subprocess.run(["tensorboard", "--logdir", "./graphs", "--port", args.port])
